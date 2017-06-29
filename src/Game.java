0d3dd5fd6c93a439e941fe1c74a3bf7bf9d61fdc
Unknown <marcelo.andrighetto@hotmail.com>
2017-06-29 16:24:07 -0300

    Function name adjust

--- src/Game.java
+++ src/Game.java
@@ -6,10 +6,12 @@ public class Game {
 	private static final int BOARD_SIZE = 15;
 	
 	public static void main(String[] args) {
-		int[][] board = Game.initBoard.apply(createBoard, BOARD_SIZE);
+		int[][] board = Game.initBoard.apply(makeBoard, BOARD_SIZE);
+		
+		// TODO fill board with ships
 	}
 	
-	public static Function<Integer,int[][]> createBoard =
+	public static Function<Integer,int[][]> makeBoard =
 			size -> new int[size][size];
 
 	public static BiFunction<Function<Integer,int[][]>, Integer, int[][]> initBoard =
